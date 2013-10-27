b39eab89e81c8b4f1fe5a11ee36f890b622addd4
Quentin330 <quentin.menini@gmail.com>
2013-10-27 23:43:48 +0100

    Ajout des classes pour les pièces et quelques commentaires

--- WebChess/src/Piece.java
+++ WebChess/src/Piece.java
@@ -1,7 +1,19 @@
 
 abstract public class Piece {
 	
-	public Piece(){
-	}
+	/**
+	 * true for black, false for white
+	 */
+	private boolean color;
+	
+	/**
+	 * position de la pièce en hauteur
+	 */
+	private int height;
+	
+	/**
+	 * position de la pièce en largeur
+	 */
+	private int width;
 	
 }
