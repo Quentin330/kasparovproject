1fc6929840e9b53a2a5fe2dfec788fbd41783dde
Quentin Menini <qmenini@emi.u-bordeaux1.fr>
2013-10-28 15:32:50 +0100

    Rajout oublie dans mouvement du roi

--- WebChess/src/King.java
+++ WebChess/src/King.java
@@ -15,49 +15,49 @@ public class King extends Piece{
 		ArrayList<Square> echecList = board.echec(this.getColor());
 		//haut
 		if (this.getHeigth()!=8){
-			if (board.isEchec(echecList, this.getHeigth()+1, this.getWidth())){
+			if (!this.isSameColor(board, this.getHeigth()+1, this.getWidth()) && board.isEchec(echecList, this.getHeigth()+1, this.getWidth())){
 				movesList.add(new Square(this.getHeigth()+1, this.getWidth()));
 			}
 		}
 		//bas
 		if (this.getHeigth()!=1){
-			if (board.isEchec(echecList, this.getHeigth()-1, this.getWidth())){
+			if (!this.isSameColor(board, this.getHeigth()-1, this.getWidth()) && board.isEchec(echecList, this.getHeigth()-1, this.getWidth())){
 				movesList.add(new Square(this.getHeigth()-1, this.getWidth()));
 			}
 		}
 		//gauche
 		if (this.getWidth()!=1){
-			if (board.isEchec(echecList, this.getHeigth(), this.getWidth()-1)){
+			if (!this.isSameColor(board, this.getHeigth(), this.getWidth()-1) && board.isEchec(echecList, this.getHeigth(), this.getWidth()-1)){
 				movesList.add(new Square(this.getHeigth(), this.getWidth()-1));
 			}
 		}
 		//droite
 		if (this.getWidth()!=8){
-			if (board.isEchec(echecList, this.getHeigth(), this.getWidth()+1)){
+			if (!this.isSameColor(board, this.getHeigth(), this.getWidth()+1) && board.isEchec(echecList, this.getHeigth(), this.getWidth()+1)){
 				movesList.add(new Square(this.getHeigth(), this.getWidth()+1));
 			}
 		}
 		//haut droite
 		if (this.getHeigth()!=8 && this.getWidth()!=8){
-			if (board.isEchec(echecList, this.getHeigth()+1, this.getWidth()+1)){
+			if (!this.isSameColor(board, this.getHeigth()+1, this.getWidth()+1) && board.isEchec(echecList, this.getHeigth()+1, this.getWidth()+1)){
 				movesList.add(new Square(this.getHeigth()+1, this.getWidth()+1));
 			}
 		}
 		//haut gauche
 		if (this.getHeigth()!=8 && this.getWidth()!=1){
-			if (board.isEchec(echecList, this.getHeigth()+1, this.getWidth()-1)){
+			if (!this.isSameColor(board, this.getHeigth()+1, this.getWidth()-1) && board.isEchec(echecList, this.getHeigth()+1, this.getWidth()-1)){
 				movesList.add(new Square(this.getHeigth()+1, this.getWidth()-1));
 			}
 		}
 		//bas droite
 		if (this.getHeigth()!=1 && this.getWidth()!=8){
-			if (board.isEchec(echecList, this.getHeigth()-1, this.getWidth()+1)){
+			if (!this.isSameColor(board, this.getHeigth()-1, this.getWidth()+1) && board.isEchec(echecList, this.getHeigth()-1, this.getWidth()+1)){
 				movesList.add(new Square(this.getHeigth()-1, this.getWidth()+1));
 			}
 		}
 		//bas gauche
 		if (this.getHeigth()!=1 && this.getWidth()!=1){
-			if (board.isEchec(echecList, this.getHeigth()-1, this.getWidth()-1)){
+			if (!this.isSameColor(board, this.getHeigth()-1, this.getWidth()-1) && board.isEchec(echecList, this.getHeigth()-1, this.getWidth()-1)){
 				movesList.add(new Square(this.getHeigth()-1, this.getWidth()-1));
 			}
 		}
